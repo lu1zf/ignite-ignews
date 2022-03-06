994fa1037bea603c9badca8a5481ea7a0a83e3b5
Luiz Silva <luizsilva.ee@gmail.com>
2022-03-05 23:12:16 -0300

    style(Header): Removes blank line

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,7 +11,6 @@ export function Header() {
           <a className={styles.active} href="">Home</a>
           <a href="">Posts</a>
         </nav>
-
         <SignInButton />
       </div>
     </header>
